8f855996fc0533096bfe193c5048b367dd53ec03
CandylandBare <94102776+CandylandBare@users.noreply.github.com>
2022-02-04 16:41:59 +0700

    Delete LAB-3 directory

--- LAB-3/lab3-2.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int num;
-    printf("Please enter an integer between 10000 and 9999999");
-    scanf("%d", &num);
-    printf("%3d,%-.3d\n", num / 1000, num % 1000);
-    return(0);
-}
